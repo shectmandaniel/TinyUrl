3c6e5b8ebead8ab79e3ca4d1ddf7bd5a70531599
shectmandaniel <shechtmandaniel@gmail.com>
2024-07-03 14:03:06 +0300

    With mongodb save users + clicks.

--- src/main/java/com/handson/tinyurl/controller/AppController.java
+++ src/main/java/com/handson/tinyurl/controller/AppController.java
@@ -2,14 +2,23 @@ package com.handson.tinyurl.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.handson.tinyurl.model.User;
+import com.handson.tinyurl.repository.UserRepository;
 import com.handson.tinyurl.service.Redis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.web.bind.annotation.*;
 import com.handson.tinyurl.model.NewTinyRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Random;
+
+import static com.handson.tinyurl.model.User.UserBuilder.anUser;
+import static com.handson.tinyurl.util.Dates.getCurMonth;
 
 @RestController
 public class AppController {
@@ -25,6 +34,32 @@ public class AppController {
     ObjectMapper om;
     @Value("${base.url}")
     String baseUrl;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public User createUser(@RequestParam String name) {
+        User user = anUser().withName(name).build();
+        user = userRepository.insert(user);
+        return user;
+    }
+
+    @RequestMapping(value = "/user/{name}", method = RequestMethod.GET)
+    public User getUser(@RequestParam String name) {
+        User user = userRepository.findFirstByName(name);
+        return user;
+    }
+
+    private void incrementMongoField(String userName, String key){
+        Query query = Query.query(Criteria.where("name").is(userName));
+        Update update = new Update().inc(key, 1);
+        mongoTemplate.updateFirst(query, update, "users");
+    }
+
 
     @RequestMapping(value = "/tiny", method = RequestMethod.POST)
     public String generate(@RequestBody NewTinyRequest request) throws JsonProcessingException {
@@ -43,6 +78,12 @@ public class AppController {
         Object tinyRequestStr = redis.get(tiny);
         NewTinyRequest tinyRequest = om.readValue(tinyRequestStr.toString(),NewTinyRequest.class);
         if (tinyRequest.getLongUrl() != null) {
+            String userName = tinyRequest.getUserName();
+            if ( userName != null) {
+                incrementMongoField(userName, "allUrlClicks");
+                incrementMongoField(userName,
+                        "shorts."  + tiny + ".clicks." + getCurMonth());
+            }
             return new ModelAndView("redirect:" + tinyRequest.getLongUrl());
         } else {
             throw new RuntimeException(tiny + " not found");
--- src/main/java/com/handson/tinyurl/model/NewTinyRequest.java
+++ src/main/java/com/handson/tinyurl/model/NewTinyRequest.java
@@ -3,7 +3,14 @@ package com.handson.tinyurl.model;
 public class NewTinyRequest {
     private  String longUrl;
 
+    private  String userName;
+
     public String getLongUrl() {
         return longUrl;
+    }
+
+
+    public String getUserName() {
+        return userName;
     }
 }
